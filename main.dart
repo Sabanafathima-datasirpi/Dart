da29a8d41309e02a6bc871230843ff6dadf6c32f
Sabanafathima-datasirpi <138755628+Sabanafathima-datasirpi@users.noreply.github.com>
2023-09-01 15:38:26 +0530

    on generate route

--- main.dart
+++ main.dart
@@ -8,41 +8,94 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyHomePage(),
+      initialRoute: '/', // Set the initial route
+      onGenerateRoute: (settings) {
+        // Define your routes and return corresponding widgets
+        switch (settings.name) {
+          case '/':
+            return MaterialPageRoute(builder: (_) => HomeScreen());
+          case '/second':
+            return MaterialPageRoute(builder: (_) => SecondScreen());
+          case '/third':
+            return MaterialPageRoute(builder: (_) => ThirdScreen());
+          default:
+            // Handle unknown routes here (e.g., show an error page)
+            return MaterialPageRoute(builder: (_) => NotFoundScreen());
+        }
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  String selectedValue = 'One'; 
-
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Dropdown Button Example'),
+        title: Text('Home'),
       ),
       body: Center(
-        child: DropdownButton<String>(
-          value: selectedValue,
-          onChanged: (newValue) {
-            setState(() {
-              selectedValue = newValue!;
-            });
+        child: ElevatedButton(
+          onPressed: () {
+            // Navigate to the second screen when the button is pressed
+            Navigator.of(context).pushNamed('/second');
           },
-          items: <String>['One', 'Two', 'THree', 'Four']
-              .map<DropdownMenuItem<String>>((String value) {
-            return DropdownMenuItem<String>(
-              value: value,
-              child: Text(value),
-            );
-          }).toList(),
+          child: Text('Go to Second Screen'),
         ),
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second Screen'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // Navigate to the third screen when the button is pressed
+            Navigator.of(context).pushNamed('/third');
+          },
+          child: Text('Go to Third Screen'),
+        ),
+      ),
+    );
+  }
+}
+
+class ThirdScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Third Screen'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // Navigate back to the home screen when the button is pressed
+            Navigator.of(context).pushNamed('/');
+          },
+          child: Text('Go Back to Home'),
+        ),
+      ),
+    );
+  }
+}
+
+class NotFoundScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Not Found'),
+      ),
+      body: Center(
+        child: Text('Page not found. Please check the URL.'),
       ),
     );
   }
