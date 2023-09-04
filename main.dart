88d452c418de0ee968f443ffffb5f95ad147754b
Sabanafathima-datasirpi <138755628+Sabanafathima-datasirpi@users.noreply.github.com>
2023-09-04 17:13:35 +0530

    header section

--- main.dart
+++ main.dart
@@ -1,51 +1,78 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: MyHomePage(),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Header section',
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  String selectedValue = 'One';
+class _HomePageState extends State<HomePage> {
+
+  final List _peopleData = List.generate(1000, (index) {
+    return {"name": "Person \#$index", "age": Random().nextInt(90) + 10};
+  });
+
+  Widget _listItem(index) {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      child: ListTile(
+        leading: Text(index.toString(), style: const TextStyle(fontSize: 18)),
+        title: Text(
+          _peopleData[index]['name'].toString(),
+          style: const TextStyle(fontSize: 18),
+        ),
+        trailing: Text(_peopleData[index]['age'].toString(),
+            style: const TextStyle(fontSize: 18, color: Colors.purple)),
+      ),
+      decoration: const BoxDecoration(
+          border: Border(bottom: BorderSide(width: 1, color: Colors.black26))),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Dropdown Button Example'),
-      ),
-      body: Center(
-        child: DropdownButtonHideUnderline(
-          child: DropdownButton<String>(
-            value: selectedValue,
-            onChanged: (newValue) {
-              setState(() {
-                selectedValue = newValue!;
-              });
-            },
-            items: <String>['One', 'Two', 'Three', 'Four']
-                .map<DropdownMenuItem<String>>((String value) {
-              return DropdownMenuItem<String>(
-                value: value,
-                child: Text(value),
-              );
-            }).toList(),
-          ),
+        appBar: AppBar(
+          title: const Text('Header section'),
         ),
-      ),
-    );
+        body: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(10),
+              color: Colors.amber,
+              child: const ListTile(
+                leading: Text('ID'),
+                title: Text('Name'),
+                trailing: Text('Age'),
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                  itemCount: _peopleData.length,
+                  itemBuilder: (_, index) {
+                    return _listItem(index);
+                  }),
+            ),
+          ],
+        ));
   }
 }
